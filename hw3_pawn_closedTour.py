a31e8a43152709ad670741f4c32b080c22662fb7
ianctw16 <ian@Iande-MacBook-Pro.local>
2017-12-08 17:13:40 +0800

    set ui

--- hw3_pawn_closedTour.py
+++ hw3_pawn_closedTour.py
@@ -1,9 +1,24 @@
 import numpy as np
+import tkinter as tk
+
+window = tk.Tk()
+frame = tk.Frame(window)
+
 n = input("n*m board n: ")  # row
 m = input("n*m board m: ")  # col
 
+window.title(n + '*' + m + 'pawnClosedTour')
 n = int(n)
 m = int(m)
+
+# window.geometry('600x400')
+
+if(m % 2 == 0 and n % 2 == 1):
+    tmp = n
+    n = m
+    m = tmp
+
+frame.pack(fill=tk.X, padx=n, pady=m)
 
 board = np.zeros((n, m))
 # print(board)
@@ -47,22 +62,21 @@ else:
     ################# !! WRONG!!! It's open tour.. HAHA ##################
     """
     for point in range(1, n*m+1):
+        print(i, j, point)
         board[i][j] = point
-        if(i == 0 and j == 0):
-            j = j + 1
-            continue
-        if(j == m-1 and control_j == 0):
-            i = i + 1
-            control_j = 1
-            continue
-        if(j == 0 and control_j == 1):
-            i = i + 1
-            control_j = 0
-            continue
-        if(control_j == 1):
-            j = j - 1
-            continue
-        if(control_j == 0):
-            j = j + 1
-            continue
 print(board)
+
+for i in range(0, n):
+    for j in range(0, m):
+        if(i == 0 and j == 0):
+            b = tk.Label(frame, text=int(board[i][j]), bg='orangered', width=5, height=2)
+        if(j == 0 and i != 0):
+            b = tk.Label(frame, text=int(board[i][j]), bg='springgreen', width=5, height=2)
+        if(i % 2 == 0 and j != 0):
+            b = tk.Label(frame, text=int(board[i][j]), bg='cyan', width=5, height=2)
+        if(i % 2 == 1 and j != 0):
+            b = tk.Label(frame, text=int(board[i][j]), bg='greenyellow', width=5, height=2)
+        b.grid(row=i, column=j)
+
+window.update()
+window.mainloop()
