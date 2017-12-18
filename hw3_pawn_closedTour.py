ca2dce807fc4724611e673aa30256e8d917d0bcb
ianctw16 <ian08160816@hotmail.com>
2017-12-18 22:18:44 +0800

    add ui

--- hw3_pawn_closedTour.py
+++ hw3_pawn_closedTour.py
@@ -1,82 +1,115 @@
 import numpy as np
 import tkinter as tk
+import time
+
+n = 0
+m = 0
+
+
+def main():
+    global n
+    global m
+    if(m % 2 == 0 and n % 2 == 1):
+        tmp = n
+        n = m
+        m = tmp
+
+    board = np.zeros((n, m))
+
+    i = 0
+    j = 0
+    control_j = 0
+    colors = ['springgreen', 'deepskyblue']
+    int_color = 0
+
+    if(n % 2 == 0):
+        # giving board the number in order.
+        for point in range(1, n*m+1):
+            board[i][j] = point
+            # ui display
+            lb = tk.Label(window,
+                          text=int(board[i][j]),
+                          height=2, width=5,
+                          bg=colors[int_color])
+            lb.grid(row=i, column=j)
+            window.update()
+            time.sleep(0.2)
+            # color control
+            if(int_color + 1 >= 2):
+                int_color = 0
+            else:
+                int_color = int_color + 1
+
+            # when j reach end of row, change line and let j decrease.
+            if(i == 0 and j == 0):
+                j = j + 1
+                continue
+            if(j == m-1 and control_j == 0):
+                i = i + 1
+                control_j = 1
+                continue
+            # when i = 1, change line
+            if(j == 1 and (i != n - 1) and (control_j == 1)):
+                i = i + 1
+                control_j = 0
+                continue
+            # normal situation j = j + 1
+            if(control_j == 0 and (i != n - 1) and (j != 0)):
+                j = j + 1
+                continue
+            # another situation j = j - 1
+            elif(control_j == 1 and (j != 0)):
+                j = j - 1
+                continue
+            # when it goes last line in the board.
+            if(i == n - 1 and (j == 1)):
+                j = 0
+                continue
+            if(j == 0 and (i in range(1, n))):
+                i = i - 1
+                continue
+    else:
+        print("Pawn's closed tour does not exist [odd*odd] board's solution.")
+
+    window.update()
+    window.mainloop()
+
+
+def getSize():
+    global n
+    global m
+    n = e1.get()
+    m = e2.get()
+    window.title(n + '*' + m + ' pawnClosedTour')
+    n = int(n)
+    m = int(m)
+    lb1.destroy()
+    lb2.destroy()
+    e1.destroy()
+    e2.destroy()
+    bt1.destroy()
+    main()
+
 
 window = tk.Tk()
-frame = tk.Frame(window)
+window.configure(background='white')
+# frame = tk.Frame(window)
 
-n = input("n*m board n: ")  # row
-m = input("n*m board m: ")  # col
+# n = input("n*m board n: ")  # row
+# m = input("n*m board m: ")  # col
 
-window.title(n + '*' + m + 'pawnClosedTour')
-n = int(n)
-m = int(m)
+lb1 = tk.Label(window, text="Board Width:", width=11, height=1, bg='gold')
+lb2 = tk.Label(window, text="Board Height:", width=11, height=1, bg='gold')
+lb1.grid(row=0)
+lb2.grid(row=1)
+e1 = tk.Entry(window)
+e2 = tk.Entry(window)
+e1.grid(row=0, column=1)
+e2.grid(row=1, column=1)
+
+bt1 = tk.Button(window, text="Submit", command=getSize, bg='lawngreen')
+bt1.grid(row=3, column=3)
+
+bt1.mainloop()
 
 # window.geometry('600x400')
-
-if(m % 2 == 0 and n % 2 == 1):
-    tmp = n
-    n = m
-    m = tmp
-
-frame.pack(fill=tk.X, padx=n, pady=m)
-
-board = np.zeros((n, m))
-# print(board)
-
-
-i = 0
-j = 0
-control_i = 0
-control_j = 0
-
-if(n % 2 == 0):
-    for point in range(1, n*m+1):
-        board[i][j] = point
-        # when j reach end of row, change line and let j decrease.
-        if(i == 0 and j == 0):
-            j = j + 1
-            continue
-        if(j == m-1 and control_j == 0):
-            i = i + 1
-            control_j = 1
-            continue
-        # when i = 1, change line
-        if(j == 1 and (i != n - 1) and (control_j == 1)):
-            i = i + 1
-            control_j = 0
-            continue
-        if(control_j == 0 and (i != n - 1) and (j != 0)):
-            j = j + 1
-            continue
-        elif(control_j == 1 and (j != 0)):
-            j = j - 1
-            continue
-        if(i == n - 1 and (j == 1)):
-            j = 0
-            continue
-        if(j == 0 and (i in range(1, n))):
-            i = i - 1
-            continue
-else:
-    """
-    ################# !! WRONG!!! It's open tour.. HAHA ##################
-    """
-    for point in range(1, n*m+1):
-        print(i, j, point)
-        board[i][j] = point
-print(board)
-
-for i in range(0, n):
-    for j in range(0, m):
-        if(i == 0 and j == 0):
-            b = tk.Label(frame, text=int(board[i][j]), bg='orangered', width=5, height=2)
-        if(j == 0 and i != 0):
-            b = tk.Label(frame, text=int(board[i][j]), bg='springgreen', width=5, height=2)
-        if(i % 2 == 0 and j != 0):
-            b = tk.Label(frame, text=int(board[i][j]), bg='cyan', width=5, height=2)
-        if(i % 2 == 1 and j != 0):
-            b = tk.Label(frame, text=int(board[i][j]), bg='greenyellow', width=5, height=2)
-        b.grid(row=i, column=j)
-
-window.update()
-window.mainloop()
--- hw3_pawn_opentour.py
+++ hw3_pawn_opentour.py
@@ -1,35 +1,108 @@
 import numpy as np
+import tkinter as tk
+import time
 
-n = input("n*m board n: ")
-m = input("n*m board m: ")
+n = 0
+m = 0
 
-n = int(n)
-m = int(m)
 
-i = 0
-j = 0
-control_i = 0
-control_j = 0
+# main program
+def main():
+    global n
+    global m
+    """
+    # preprocess board's width and height
+    if(m % 2 == 0 and n % 2 == 1):
+        tmp = n
+        n = m
+        m = tmp
+    """
+    # creat board
+    board = np.zeros((n, m))
 
-board = np.zeros((n, m))
+    i = 0
+    j = 0
+    control_j = 0
+    colors = ['springgreen', 'deepskyblue']
+    int_color = 0
 
-for point in range(1, n*m+1):
-    board[i][j] = point
-    if(i == 0 and j == 0):
-        j = j + 1
-        continue
-    if(j == m-1 and control_j == 0):
-        i = i + 1
-        control_j = 1
-        continue
-    if(j == 0 and control_j == 1):
-        i = i + 1
-        control_j = 0
-        continue
-    if(control_j == 1):
-        j = j - 1
-        continue
-    if(control_j == 0):
-        j = j + 1
-        continue
-print(board)
+    # main pawn's tour
+    if(n % 2 == 0):
+        # giving board the number in order.
+        for point in range(1, n*m+1):
+            board[i][j] = point
+            # ui display
+            lb = tk.Label(window,
+                          text=int(board[i][j]),
+                          height=2, width=5,
+                          bg=colors[int_color])
+            lb.grid(row=i, column=j)
+            window.update()
+            time.sleep(0.2)
+
+            # color control
+            if(int_color + 1 >= 2):
+                int_color = 0
+            else:
+                int_color = int_color + 1
+
+            if(i == 0 and j == 0):
+                j = j + 1
+                continue
+            if(j == m-1 and control_j == 0):
+                i = i + 1
+                control_j = 1
+                continue
+            if(j == 0 and control_j == 1):
+                i = i + 1
+                control_j = 0
+                continue
+            if(control_j == 1):
+                j = j - 1
+                continue
+            if(control_j == 0):
+                j = j + 1
+                continue
+    else:
+        print("Pawn's closed tour does not exist [odd*odd] board's solution.")
+
+    window.update()
+    window.mainloop()
+
+
+#  get board size and ui display
+def getSize():
+    global n
+    global m
+    n = e1.get()
+    m = e2.get()
+    window.title(n + '*' + m + " pawn's Open Tour")
+    n = int(n)
+    m = int(m)
+    lb1.destroy()
+    lb2.destroy()
+    e1.destroy()
+    e2.destroy()
+    bt1.destroy()
+    main()
+
+
+# ui widgets
+window = tk.Tk()
+window.configure(background='white')
+
+lb1 = tk.Label(window, text="Board Width:", width=11, height=1, bg='gold')
+lb2 = tk.Label(window, text="Board Height:", width=11, height=1, bg='gold')
+lb1.grid(row=0)
+lb2.grid(row=1)
+e1 = tk.Entry(window)
+e2 = tk.Entry(window)
+e1.grid(row=0, column=1)
+e2.grid(row=1, column=1)
+
+bt1 = tk.Button(window, text="Submit", command=getSize, bg='lawngreen')
+bt1.grid(row=3, column=3)
+
+bt1.mainloop()
+
+# window.geometry('600x400')
